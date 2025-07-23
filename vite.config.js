1725649ae530fbdcadc77592a2d238cebbd4196d
zl <coolzhongliang@126.com>
2025-07-23 17:44:45 +0800

    tes ts

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
+import { fileURLToPath } from 'url'
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      treeshake: true,
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+          if (id.includes('modules')) {
+            return id.toString().split('modules/')[1].split('/')[0].toString()
+          }
+        },
+        chunkFileNames: (chunkInfo) => {
+          const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : []
+          const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]'
+          return `js/${fileName}/[name].[hash].js`
+        }
+      }
+    }
+  }
 })
